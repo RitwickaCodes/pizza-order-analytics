6a180fe9e0d5cbc6bee1a35a19728b4e2b45950f
RitwickaCodes <RitwickaCodes@users.noreply.github.com>
2024-07-13 21:42:36 +0530

    Initial commit with project structure and files

--- pizza_sales/06_Revenue_Contribution_Size.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    p.size, SUM(od.quantity * p.price) AS total_revenue
-FROM
-    order_details od
-        JOIN
-    pizzas p ON od.pizza_id = p.pizza_id
-GROUP BY p.size
-ORDER BY total_revenue DESC;
